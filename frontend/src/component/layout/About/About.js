eabdc7bd573c0d1a662d2160bc86e2a0965981fc
Harsh Verdhan Raj <harsh.hvr0@gmail.com>
2023-10-31 04:57:31 +0530

    New Updates

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -1,56 +1,128 @@
 import React from "react";
-import "./aboutSection.css";
-import { Typography, Avatar } from "@material-ui/core";
+import {} from "react-router-dom";
+import "./About.css";
 // import YouTubeIcon from "@material-ui/icons/YouTube";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import InstagramIcon from "@material-ui/icons/Instagram";
+import { FaGithub, FaInstagram } from "react-icons/fa";
 const About = () => {
   // const visitInstagram = () => {
   //   window.location = "https://instagram.com/meabhisingh";
   // };
   return (
-    <div className="aboutSection">
-      <div></div>
-      <div className="aboutSectionGradient"></div>
-      <div className="aboutSectionContainer">
-        <Typography component="h1">About Us</Typography>
+    <>
+      <div className="about-section">
+        <div className="about-section-inner">
+          <div className="title">About Us</div>
 
-        <div>
-          <div>
-            <Avatar
-              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/drgu0tbi8/image/upload/v1698349043/avatars/ovbdubdxq1ii8bifuttl.png"
-              alt="Founder"
-            />
-            <Typography>Harsh Verdhan Raj</Typography>
-            {/* <Button onClick={visitInstagram} color="primary">
-              Visit Instagram
-            </Button> */}
-            <br />
-            <span>
-              This is a sample wesbite made by @meharshverdhanraj. Only with the
-              purpose to learn MERN Stack.
-            </span>
+          <div className="article">
+            We offer a wide variety of products with great deals on products,
+            easy ordering options, secure payment options for your convenience
+            and let you find products easily.
           </div>
-          <div className="aboutSectionContainer2">
-            <Typography component="h2">Our Brands</Typography>
-            {/* <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
-              target="blank"
-            >
-              <YouTubeIcon className="youtubeSvgIcon" />
-            </a> */}
 
-            <a href="https://github.com/harshhvr" target="blank">
-              <GitHubIcon className="githubSvgIcon" />
-            </a>
-            <a href="https://www.instagram.com/harshhvr13/" target="blank">
-              <InstagramIcon className="instagramSvgIcon" />
-            </a>
+          <div className="article">
+            <div className="title">Team of Developers</div>
+
+            <ul className="card-items">
+              <li className="card-item">
+                <div className="card">
+                  <div className="card-head">
+                    <img
+                      src="https://avatars.githubusercontent.com/u/120925639?v=4"
+                      alt="Profile Pic"
+                    />
+                  </div>
+
+                  <div className="card-body">Devraj Trivedi</div>
+
+                  <div className="card-foot">
+                    <div className="social-icon-links">
+                      <a
+                        href="https://github.com/devrajtrivedi"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithub />
+                      </a>
+                      <a
+                        href="https://www.instagram.com/devraj._trivedi"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaInstagram />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li className="card-item">
+                <div className="card">
+                  <div className="card-head">
+                    <img
+                      src="https://res.cloudinary.com/drgu0tbi8/image/upload/v1698349043/avatars/ovbdubdxq1ii8bifuttl.png"
+                      alt="Profile Pic"
+                    />
+                  </div>
+
+                  <div className="card-body">Harsh Verdhan Raj</div>
+
+                  <div className="card-foot">
+                    <div className="social-icon-links">
+                      <a
+                        href="https://github.com/harshhvr"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithub />
+                      </a>
+                      <a
+                        href="https://www.instagram.com/harshhvr13"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaInstagram />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li className="card-item">
+                <div className="card">
+                  <div className="card-head">
+                    <img
+                      src="https://avatars.githubusercontent.com/u/64636872?v=4"
+                      alt="Profile Pic"
+                    />
+                  </div>
+
+                  <div className="card-body">Shlok Jaiswal</div>
+
+                  <div className="card-foot">
+                    <div className="social-icon-links">
+                      <a
+                        href="https://github.com/shlokjaiswal586"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaGithub />
+                      </a>
+                      <a
+                        href="https://www.instagram.com/_shlok__jaiswal"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaInstagram />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
